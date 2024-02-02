eca3609362e1e151cd50b585691ffe2d3a1a7709
EKTA549 <ekta@v-learning.in>
2024-02-02 10:17:00 +0530

    first commi

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebaselogin/login_screen.dart';
 import 'package:firebaselogin/signup_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -11,9 +12,13 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // Create an instance of AuthProvider here
+  MyAuthProvider.AuthProvider authProvider = MyAuthProvider.AuthProvider();
+
   runApp(
-    ChangeNotifierProvider<MyAuthProvider.AuthProvider>(
-      create: (BuildContext context) => MyAuthProvider.AuthProvider(),
+    ChangeNotifierProvider<MyAuthProvider.AuthProvider>.value(
+      value: authProvider,
       child: const MyApp(),
     ),
   );
@@ -24,23 +29,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<MyAuthProvider.AuthProvider>(
-      create: (BuildContext context) => MyAuthProvider.AuthProvider(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Consumer<MyAuthProvider.AuthProvider>(
-          builder: (context, authProvider, child) {
-            // Check if the user is authenticated
-            if (authProvider.user != null) {
-              // User is authenticated, navigate to the home screen
-              // Replace HomeScreen with your actual home screen
-              return const HomeScreen();
-            } else {
-              // User is not authenticated, show the login screen
-              return const SignUpScreen();
-            }
-          },
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Consumer<MyAuthProvider.AuthProvider>(
+        builder: (context, authProvider, child) {
+          // Check if the user is authenticated
+          if (authProvider.user != null) {
+            // User is authenticated, navigate to the home screen
+            // Replace HomeScreen with your actual home screen
+            return const LoginScreen();
+          } else {
+            // User is not authenticated, show the login screen
+            return const SignUpScreen();
+          }
+        },
       ),
     );
   }
